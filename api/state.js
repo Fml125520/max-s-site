d2351dd421cd050d1670b2ece43a5156db0a5271
Fml125520 <zyad.hazem55.zh@gmail.com>
2026-09-03 01:14:57 +0300

    Add 'delivery' key to allowed keys set

--- api/state.js
+++ api/state.js
@@ -12,7 +12,7 @@ const redis = new Redis({
 });
 
 // المفاتيح المسموح بالتعامل معها بس (حماية بسيطة عشان محدش يبعت مفتاح عشوائي)
-const ALLOWED_KEYS = new Set(['menu', 'users', 'whatsapp', 'payment']);
+const ALLOWED_KEYS = new Set(['menu', 'users', 'whatsapp', 'payment', 'delivery']);
 
 // بادئة لكل مفتاح عشان لو استخدمت نفس الـ KV database لمشاريع تانية
 const PREFIX = 'maxez:';
